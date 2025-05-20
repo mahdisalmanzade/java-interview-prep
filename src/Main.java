6e43ec5839a7a25c663d1ba0379acd0b9c5d82e2
Mahdi Salmanizadehgan <mahdisalmanizadehgan@gmail.com>
2025-05-20 22:41:30 +0330

    feat: Declaring strings using literal approach and new keyword

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String a = "some text";
+        String b = "some text";
+        String c = new String("some text");
+
+        System.out.println(a == b); // true same object reference
+        System.out.println(a.equals(b)); // true
+        System.out.println(a.equals(c)); // true comparing values not references
+        System.out.println(a == c); // false "new" keyword create a new object in the heap
     }
 }
